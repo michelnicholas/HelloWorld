a0219a71802b9b2236bb9536965b02600185ca94
Michel Nicholas <michelnicholas1993@gmail.com>
2022-02-24 20:37:27 -0500

    blackjack javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+ 
+let person = {
+
+    name: 'Mitch',
+    age: 30
+
+};
+// Dot Notation
+person.name = 'Nicholas';
+
+// Bracket Notation
+person['name'] = 'Mary';
+
+
+console.log(person.name);
+
